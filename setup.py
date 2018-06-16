0d47f4bcc7edf1720b84dbabf62813e5136d0fcf
hmallen <allenhm@gmail.com>
2018-06-16 04:34:41 -0500

    Removed typo.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='coinmarketcap_tracker',
-    version='0.1dev8',
+    version='0.1dev9',
     author='Hunter M. Allen',
     author_email='allenhm@gmail.com',
     license='MIT',
